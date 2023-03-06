705fa7a4c0fda7fe16608576872d1bc8db9b7b4e
anhhuy12345 <thienhung190492@gmail.com>
2023-03-06 08:26:19 +0700

    bt

--- ss7_spring_boot/exercise/blog_application/src/main/java/com/ss7/repository/ICategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ss7.repository;
-
-import com.ss7.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ICategoryRepository extends JpaRepository<Category, Integer> {
-}
