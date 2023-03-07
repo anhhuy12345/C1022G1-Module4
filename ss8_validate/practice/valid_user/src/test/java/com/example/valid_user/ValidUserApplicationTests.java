04ad1105d7551e8067e7a83afa8787e3c60f74f9
anhhuy12345 <thienhung190492@gmail.com>
2023-03-07 19:24:04 +0700

    bt

---
+++ ss8_validate/practice/valid_user/src/test/java/com/example/valid_user/ValidUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.valid_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
